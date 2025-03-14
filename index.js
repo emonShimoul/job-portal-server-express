b59d3ab7d4fc77ddd3a81260f670070b4f77bdfd
emonShimoul <emonshimoul@gmail.com>
2025-03-14 15:22:23 +0600

    load a single job using unique id

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 require("dotenv").config();
 
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 app.use(cors());
 app.use(express.json());
@@ -35,9 +35,17 @@ async function run() {
 
     // jobs related apis
     const jobsCollection = client.db("jobPortal").collection("jobs");
+
     app.get("/jobs", async (req, res) => {
       const cursor = jobsCollection.find();
       const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/jobs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobsCollection.findOne(query);
       res.send(result);
     });
   } finally {
