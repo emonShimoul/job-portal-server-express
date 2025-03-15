120b75f109748599afff5bb82439f2969aafe869
emonShimoul <emonshimoul@gmail.com>
2025-03-16 03:48:22 +0600

    save new job to database

--- index.js
+++ index.js
@@ -52,6 +52,12 @@ async function run() {
       res.send(result);
     });
 
+    app.post("/jobs", async (req, res) => {
+      const newJob = req.body;
+      const result = await jobsCollection.insertOne(newJob);
+      res.send(result);
+    });
+
     // job application apis
 
     app.get("/job-application", async (req, res) => {
