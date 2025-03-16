125fc46a7ba71519d7944b1a55b3ba14af3257ca
emonShimoul <emonshimoul@gmail.com>
2025-03-17 01:53:21 +0600

    count job application in the specific job

--- index.js
+++ index.js
@@ -89,6 +89,29 @@ async function run() {
     app.post("/job-applications", async (req, res) => {
       const application = req.body;
       const result = await jobApplicationsCollection.insertOne(application);
+
+      // Not the best way (use aggregate)
+      // skip --> it
+      // for added job application count in the specific job
+      const id = application.job_id;
+      const query = { _id: new ObjectId(id) };
+      const job = await jobsCollection.findOne(query);
+      let newCount = 0;
+      if (job.applicationCount) {
+        newCount = job.applicationCount + 1;
+      } else {
+        newCount = 1;
+      }
+
+      // now update the job info
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          applicationCount: newCount,
+        },
+      };
+
+      const updateResult = await jobsCollection.updateOne(filter, updatedDoc);
       res.send(result);
     });
   } finally {
